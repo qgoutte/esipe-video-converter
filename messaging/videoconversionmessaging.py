781fee7c99e136c4e02198577b53f27151e44f5a
Quentin Goutte <quentin.goutte@etu.u-pec.fr>
2018-12-12 21:16:08 +0100

    Begin pubsub implementation

--- messaging/videoconversionmessaging.py
+++ messaging/videoconversionmessaging.py
@@ -1,5 +1,5 @@
 
-import pika
+import time
 from google.cloud import pubsub_v1
 from threading import Thread
 import logging
@@ -7,9 +7,7 @@ import json
 import queue
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
-logging.getLogger("pika").setLevel(logging.INFO)
-
-
+#logging.getLogger("pika").setLevel(logging.INFO)
 
 # rabbitmqadmin -H localhost -u ezip -p pize -V ezip purge queue name=video-conversion-queue
 # rabbitmqadmin -H localhost -u ezip -p pize -V ezip get queue=video-conversion-queue
@@ -36,27 +34,32 @@ class VideoConversionMessaging(Thread):
 #        self.start()
 
     def __init__(self, _config_, converting_service):
-        #https://cloud.google.com/pubsub/docs/quickstart-client-libraries#pubsub-subscribe-python
+        project_id = _config_.get_project_id()
+        subscription_name = _config_.get_subcription_name()
         Thread.__init__(self)
         subscriber = pubsub_v1.SubscriberClient()
         subscription_path = subscriber.subscription_path(project_id, subscription_name)
+        def callback(message):
+            logging.info("Received message :".format(message))
+            message.ack()
+        subscriber.subscribe(subscription_path, callback=callback)
+        logging.info("Listen message on :".format(subscription_path))
+        while True:
+            time.sleep(60)
 
-
-
-
-        self.channel = self.connection.channel()
-        self.rmq = _config_.get_messaging_conversion_queue()
+        #self.channel = self.connection.channel()
+        #self.rmq = _config_.get_messaging_conversion_queue()
         # self.channel.basic_consume(self.on_message, self.rmq, no_ack=True)
-        self.converting_service = converting_service
-        self.consuming = "_CONSUMING_"
-        self.rendez_vous = queue.Queue(1)
-        self.pause = queue.Queue(1)
-        self.start()
+        #self.converting_service = converting_service
+        #self.consuming = "_CONSUMING_"
+        #self.rendez_vous = queue.Queue(1)
+        #self.pause = queue.Queue(1)
+        #self.start()
 
 
 
-    def run(self):
-        while True : # "_CONSUMING_" == self.consuming :
+    #def run(self):
+     #   while True : # "_CONSUMING_" == self.consuming :
 #            logging.info("Starts consuming on message bus before RDV")
 #            self.channel.start_consuming()
 #            logging.info("WAITING Rendez-Vous")
@@ -64,50 +67,48 @@ class VideoConversionMessaging(Thread):
 #            logging.info("Rendez-Vous")
 #            self.channel = self.connection.channel()
 #            self.channel.basic_consume(self.on_message, self.rmq, no_ack=True)
-            if "_CONSUMING_" == self.consuming :
-                method, prop, body = self.channel.basic_get(self.rmq, no_ack=True)
-                if body :
-                    self._on_message_(body)
-                    pass
-                else :
-                    try :
-                        self.pause.get(timeout=1)
-                    except queue.Empty :
-                        pass
+       #     if "_CONSUMING_" == self.consuming :
+      #          method, prop, body = self.channel.basic_get(self.rmq, no_ack=True)
+        #        if body :
+         #           self._on_message_(body)
+          #          pass
+           #     else :
+            #        try :
+             #           self.pause.get(timeout=1)
+              #      except queue.Empty :
+               #         pass
                     # self._on_message_(message)
 
 
-    def on_message(self, channel, method_frame, header_frame, body):
-        logging.info(body)
+    #def on_message(self, channel, method_frame, header_frame, body):
+        #logging.info(body)
         # logging.info('id = %s, URI = %s', body["id"], body['originPath'])
         # logging.info('URI = %s', body['originPath'])
-        logging.info('URI = %s', body.decode())
-        convert_request = json.loads(body.decode())
-        logging.info(convert_request)
-        self.converting_service.convert(convert_request["id"], convert_request['originPath'])
+        #logging.info('URI = %s', body.decode())
+        #convert_request = json.loads(body.decode())
+        #logging.info(convert_request)
+        #self.converting_service.convert(convert_request["id"], convert_request['originPath'])
 
-    def _on_message_(self,  body):
-        logging.info(body)
+    #def _on_message_(self,  body):
+        #logging.info(body)
         # logging.info('id = %s, URI = %s', body["id"], body['originPath'])
         # logging.info('URI = %s', body['originPath'])
-        logging.info('URI = %s', body.decode())
-        convert_request = json.loads(body.decode())
-        logging.info(convert_request)
-        self.converting_service.convert(convert_request["id"], convert_request['originPath'])
+        #logging.info('URI = %s', body.decode())
+        #convert_request = json.loads(body.decode())
+        #logging.info(convert_request)
+        #self.converting_service.convert(convert_request["id"], convert_request['originPath'])
 
-
-
-    def stop_consuming(self):
-        logging.info("Stops consuming on message bus")
+    #def stop_consuming(self):
+        #logging.info("Stops consuming on message bus")
         # self.channel.stop_consuming()
-        self.consuming = "_IDLE_"
+        #self.consuming = "_IDLE_"
 
-    def start_consuming(self):
-        logging.info("Starts consuming on message bus")
+    #def start_consuming(self):
+        #logging.info("Starts consuming on message bus")
         #self.channel.start_consuming()
         # self.rendez_vous.put("_CONSUMING_")
-        self.consuming = "_CONSUMING_"
+        #self.consuming = "_CONSUMING_"
         # self.start()
 
-    def is_consuming(self):
-        return self.consuming
+   #def is_consuming(self):
+        #return self.consuming
